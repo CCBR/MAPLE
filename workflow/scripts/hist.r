98eab1269d35cb9796baba2d0674e3d1269f080a
slsevilla <slsevilla@gmail.com>
2022-10-27 14:13:08 -0400

    pull pre-calc'ed lengths from file

--- workflow/scripts/hist.r
+++ workflow/scripts/hist.r
@@ -12,16 +12,13 @@ output_png <- args[3]
 #input <- "~/../../../Volumes/ccbr1214/analysis/7_ARPE_5u_MNase/results/03_aligned/02_bed/7_ARPE_5u_MNase.hg19.120-140.2500_ALUdepleted.bed"
 #output_csv <- "~/../../../Volumes/ccbr1214/analysis/7_ARPE_5u_MNase/results/03_aligned/03_histograms/7_ARPE_5u_MNase.hg19.length_hist.120-140.2500_ALUdepleted.csv"
 #output_png <- "~/../../../Volumes/ccbr1214/analysis/7_ARPE_5u_MNase/results/03_aligned/03_histograms/7_ARPE_5u_MNase.hg19.length_hist.120-140.2500_ALUdepleted.png"
-#input="~/../../../Volumes/ccbr1214/v1.0/test_3/results/03_aligned/02_bed/Sample6.mapped.bed"
-#output_csv="~/../../../Volumes/ccbr1214/v1.0/test_3/results/03_aligned/03_histograms/Sample6.length_hist_all.csv"
-#output_png="~/../../../Volumes/ccbr1214/v1.0/test_3/results/03_aligned/03_histograms/Sample6.length_hist_all.png"
+#input="~/../../../Volumes/Zhurkin-20/analysis/8_NB26_5_200/results/tmp/histo_frags/tmp2.bed"
+#output_csv="~/../../../Volumes/Zhurkin-20/analysis/8_NB26_5_200/results/03_aligned/03_histograms/8_NB26_5_200.hg19.length_hist.all.csv"
+#output_png="~/../../../Volumes/Zhurkin-20/analysis/8_NB26_5_200/results/03_aligned/03_histograms/8_NB26_5_200.hg19.length_hist.all.png"
 
 # read in rawdata subset
-data <- read.csv(input, header=FALSE, sep = '\t')[,1:3]
-colnames(data) <- c('Chrom', 'Start', 'End')
-
-#calculate length
-data$Length <- data$End - data$Start
+data <- read.csv(input, header=FALSE, sep = ' ')
+colnames(data) <- c('Chrom', 'Start', 'End', 'Length')
 
 # create histogram, save
 p1 <- ggplot() + geom_histogram(data = data, aes(x=Length), color = 'black', fill = 'white', binwidth = 1, size = 1) +
